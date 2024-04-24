beb39a0e57531355b80e44ffb8836839492285cc
ffauziee <142006451+ffauziee@users.noreply.github.com>
2024-04-24 10:41:04 +0700

    initial commit

---
+++ Praktek/Week8/src/PostfixMain.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PostfixMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String P, Q;
+        System.out.print("Masukkan ekspresi matematika (infix): ");
+        Q=sc.nextLine();
+        Q=Q.trim();
+        Q=Q+")";
+
+        int total = Q.length();
+        Postfix10 post = new Postfix10(total);
+        P=post.konversi(Q);
+        System.out.println("Postfix: " + P);
+    }
+}
